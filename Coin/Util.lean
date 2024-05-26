2cf469b9734cdaaf336f6d9d6145e704738ad506
ljcleo <ljcleo@qq.com>
2024-05-26 20:34:53 +0800

    remove util.lean

--- Coin/Util.lean
+++
@@ -1,8 +0,0 @@
-import Coin.Common
-
-open AddSubgroup Set SetLike Unique
-
-variable {G : Type*} [Fintype G] [AddCommGroup G]
-
-theorem subgroup_ncard_lt_ncard_of_lt {H₁ H₂ : AddSubgroup G} (h : H₁ < H₂) :
-    H₁.carrier.ncard < H₂.carrier.ncard := ncard_lt_ncard h (toFinite _)
