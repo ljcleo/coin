67970a74db2f5c2fb5a59d22ccfca7b19514b195
ljcleo <ljcleo@qq.com>
2024-06-21 22:25:38 +0800

    update lean version start adding coin case

--- lakefile.lean
+++ lakefile.lean
@@ -9,7 +9,7 @@ package «coin» where
   -- add any additional package configuration options here
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+  "https://github.com/leanprover-community/mathlib4.git" @ "stable"
 
 @[default_target]
 lean_lib «Coin» where
